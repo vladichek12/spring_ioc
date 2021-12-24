c660843a71f959573f2f5c0c11dca4a1e455ecba
vlad <misharublov@gmail.com>
2021-12-25 00:07:57 +0300

    git commit -m "Commit message."

---
+++ src/main/java/DbMovieRepository.java
@@ -0,0 +1,8 @@
+import java.util.List;
+
+public class DbMovieRepository implements MovieRepository{
+    @Override
+    public List<Movie> findAll() {
+        return null;
+    }
+}
---
+++ src/main/java/MovieReport.java
@@ -0,0 +1,5 @@
+public class MovieReport {
+    void generate(){
+
+    }
+}

53428e3ede205f198c2fb6af17f1a7433c1b0d41
vlad <misharublov@gmail.com>
2021-12-25 23:38:35 +0300

    git commit -m "Commit message."

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,11 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class App {
 
     public static void main(String[] args) {
-        DbMovieRepository dbMovieRepository = new DbMovieRepository();
+        ApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"applicationContext.tld"});
+        DbMovieRepository dbMovieRepository = context.getBean(DbMovieRepository.class);
         MovieReport movieReport = new MovieReport(dbMovieRepository);
         movieReport.generate();
     }
--- src/main/java/MovieReport.java
+++ src/main/java/MovieReport.java
@@ -1,4 +1,3 @@
-import java.awt.*;
 import java.util.List;
 
 public class MovieReport {
